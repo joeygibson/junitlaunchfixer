04f4ea960befe2676358271928dc08e649229e18
therealjoeygibson <therealjoeygibson@8c21c1a6-a3c5-11de-b195-ab392a2a73f5>
2009-09-17 20:17:26 +0000

    First import of code.

---
+++ trunk/src/com/joeygibson/eclipse/junitlaunchfixer/preferences/JUnitLaunchFixerPreferencePage.java
@@ -0,0 +1,52 @@
+package com.joeygibson.eclipse.junitlaunchfixer.preferences;
+
+import org.eclipse.jface.preference.*;
+import org.eclipse.ui.IWorkbenchPreferencePage;
+import org.eclipse.ui.IWorkbench;
+
+import com.joeygibson.eclipse.junitlaunchfixer.Activator;
+
+/**
+ * This class represents a preference page that is contributed to the
+ * Preferences dialog. By subclassing <samp>FieldEditorPreferencePage</samp>, we
+ * can use the field support built into JFace that allows us to create a page
+ * that is small and knows how to save, restore and apply itself.
+ * <p>
+ * This page is used to modify preferences only. They are stored in the
+ * preference store that belongs to the main plug-in class. That way,
+ * preferences can be accessed directly via the preference store.
+ */
+
+public class JUnitLaunchFixerPreferencePage
+	extends FieldEditorPreferencePage
+	implements IWorkbenchPreferencePage
+{
+	public JUnitLaunchFixerPreferencePage()
+	{
+		super(GRID);
+		setPreferenceStore(Activator.getDefault().getPreferenceStore());
+		setDescription("Preferences for maximum heap settings for launchers");
+	}
+
+	/**
+	 * Creates the field editors. Field editors are abstractions of the common
+	 * GUI blocks needed to manipulate various types of preferences. Each field
+	 * editor knows how to save and restore itself.
+	 */
+	public void createFieldEditors()
+	{
+		addField(new StringFieldEditor(PreferenceConstants.P_MAX_HEAP, "&Max Heap Size:", getFieldEditorParent()));
+		addField(new BooleanFieldEditor(PreferenceConstants.P_UPDATE_EXISTING_LAUNCHERS, "&Update Existing Launchers?",
+				getFieldEditorParent()));
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.ui.IWorkbenchPreferencePage#init(org.eclipse.ui.IWorkbench)
+	 */
+	public void init(IWorkbench workbench)
+	{
+	}
+}
---
+++ trunk/src/com/joeygibson/eclipse/junitlaunchfixer/preferences/PreferenceConstants.java
@@ -0,0 +1,11 @@
+package com.joeygibson.eclipse.junitlaunchfixer.preferences;
+
+/**
+ * Constant definitions for plug-in preferences
+ */
+public class PreferenceConstants
+{
+	public static final String P_MAX_HEAP = "maxHeap";
+
+	public static final String P_UPDATE_EXISTING_LAUNCHERS = "override";
+}
