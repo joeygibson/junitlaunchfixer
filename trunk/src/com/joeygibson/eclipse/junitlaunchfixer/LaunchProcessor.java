2d326e84a5cd6e133d65d1110049c10dd5a9ad6e
therealjoeygibson <therealjoeygibson@8c21c1a6-a3c5-11de-b195-ab392a2a73f5>
2009-09-18 03:16:50 +0000

    I added the one-time updating of existing launchers. It is disabled by default, since I haven't added the prompting yet. If you go into preferences and enabled it, the next time you start Eclipse, all JUnit launchers will be updated.

--- trunk/src/com/joeygibson/eclipse/junitlaunchfixer/LaunchProcessor.java
+++ trunk/src/com/joeygibson/eclipse/junitlaunchfixer/LaunchProcessor.java
@@ -13,28 +13,20 @@ import java.util.regex.Pattern;
 
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.debug.core.ILaunchConfiguration;
-import org.eclipse.debug.core.ILaunchConfigurationListener;
 import org.eclipse.debug.core.ILaunchConfigurationWorkingCopy;
 import org.eclipse.jface.preference.IPreferenceStore;
 
 import com.joeygibson.eclipse.junitlaunchfixer.preferences.PreferenceConstants;
 
-public class MinervaLaunchConfigurationListener
-	implements ILaunchConfigurationListener
+public class LaunchProcessor 
 {
-	protected Pattern pattern = Pattern.compile("-Xmx(\\d+\\w)");
+	protected static Pattern pattern = Pattern.compile("-Xmx\\d+\\w");
 
 	private static final String VMARGS_KEY = "org.eclipse.jdt.launching.VM_ARGUMENTS";
 
 	private static final String TEST_KIND_KEY = "org.eclipse.jdt.junit.TEST_KIND";
-
-	@Override
-	public void launchConfigurationAdded(ILaunchConfiguration config)
-	{
-		processVmArgs(config);
-	}
-
-	protected void processVmArgs(ILaunchConfiguration config)
+	
+	public static void processVmArgs(ILaunchConfiguration config)
 	{
 		IPreferenceStore store = Activator.getDefault().getPreferenceStore();
 
@@ -55,7 +47,7 @@ public class MinervaLaunchConfigurationListener
 
 			if (matcher.find())
 			{
-				vmArgs = matcher.replaceFirst(maxHeap);
+				vmArgs = matcher.replaceFirst("-Xmx" + maxHeap);
 			}
 			else
 			{
@@ -71,15 +63,5 @@ public class MinervaLaunchConfigurationListener
 		{
 			e.printStackTrace();
 		}
-	}
-
-	@Override
-	public void launchConfigurationChanged(ILaunchConfiguration config)
-	{
-	}
-
-	@Override
-	public void launchConfigurationRemoved(ILaunchConfiguration config)
-	{
 	}
 }
